b8008d412fddee91c6e0bd198d12c91ab40cad74
david mark <markkariuki17@gmail.com>
2018-01-08 16:16:38 +0300

    add the flask-login extension

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Role
 from  flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 
 #creating the app instance
