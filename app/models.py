6355871f22cc194330f0faa2901b80b44ba8a914
david mark <markkariuki17@gmail.com>
2018-01-08 15:06:36 +0300

    add the role class

--- app/models.py
+++ app/models.py
@@ -5,5 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
     def __repr__(self):
         return f'User {self.username}'
