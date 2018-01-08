60cae2b09e6b312c9ea0704801edebc0b5ca6651
david mark <markkariuki17@gmail.com>
2018-01-08 15:11:41 +0300

    added the relationship

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
 
 
 class Role(db.Model):
@@ -11,6 +13,8 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -11,7 +11,6 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
-
+    return dict(app = app,db = db,User = User, Role = Role )
 if __name__ == '__main__':
     manager.run()
