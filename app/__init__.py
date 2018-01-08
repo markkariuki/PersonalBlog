d5f4326422c7eecb457f0fa4cf9ba294c0c035ac
david mark <markkariuki17@gmail.com>
2018-01-08 16:06:44 +0300

    add the auth foldeer and initialize it

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ db = SQLAlchemy()
 def create_app(config_name):
 
     app = Flask(__name__)
+    #creating the auth configurations
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
