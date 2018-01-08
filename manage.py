e91fbf96d1d57b431b330ffea5b67dd9dcf563f3
david mark <markkariuki17@gmail.com>
2018-01-08 12:54:19 +0300

    add the flask-script and initialize it

--- manage.py
+++ manage.py
@@ -1,7 +1,13 @@
-from app import create_app
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
 
 #creating the app instance
 app = create_app ('development')
+
+
+Manager = Manager(app)
+manager.add_command('server',Server)
 
 if __name__ == '__main__':
     app.run()
