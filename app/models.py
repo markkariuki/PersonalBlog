250ff036c3795d7703ecf30a264aca0220659dc9
david mark <markkariuki17@gmail.com>
2018-01-08 15:56:13 +0300

    add the user aythentication

--- app/models.py
+++ app/models.py
@@ -8,6 +8,18 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
      pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        sreturn check_password_hash(self.pass_secure,password)
+
 
 
 class Role(db.Model):
