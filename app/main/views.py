55785638d1fb5a691a2289cf8ebe369b47b0d699
david mark <markkariuki17@gmail.com>
2018-01-09 09:49:35 +0300

    add the model configurations

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,19 @@
 from flask import render_template
 from . import main
 from flask_login import login_required
+from forms import login_form
 
 #views
-@main.route('/')
-def index():
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    return render_template('index.html')
+       flash('Invalid username or Password')
 
-@main.route('/c/')
-@login_required
-def new_review(id):
-    pass
+   title = "Blog login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from . import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
