ca83568715d104d7d0df3cc019f4e9ba43bcee23
david mark <markkariuki17@gmail.com>
2018-01-15 14:43:44 +0300

    added on the subscription form

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,3 +8,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    intrested_in = SelectField('Intrested In', choices=[('Women', 'Women'),('Men','Men'),('Both','Both')],validators = [Required()])
+    age = IntegerField('Age',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+    
--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from . import main
 from flask_login import login_required
 from flask_login import login_required, current_user
+from .forms import RegistrationForm, LoginForm
 
 from .. import db
 
@@ -11,6 +12,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Blog'
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    return render_template('index.html',title = title)
+        flash('Invalid username or Password')
+    return render_template('index.html', login_form = login_form )
